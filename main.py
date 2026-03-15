97ef77f048510ee647f2a3855619fc88b5d64d2e
Oleksandr <flytek07@gmail.com>
2026-03-15 23:09:48 +0200

    Import os module for environment variable access

--- main.py
+++ main.py
@@ -74,5 +74,6 @@ def index():
         return render_template('index.html', results=data_store["table_data"])
 
 if __name__ == '__main__':
+    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
